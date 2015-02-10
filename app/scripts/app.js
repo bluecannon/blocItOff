b0cb4b162b43d82d239afcbdfca3fea34c7123ad
Harry Hsu <hhsu@mindspring.com>
2015-02-10 23:47:58 +0000

    Add fixes to blocItOff. 2-10-2015

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,17 @@
 /* Before we can use AngularFire with dependency injection, we need to register firebase as a module in our app. Harry 2-08-2015*/
-var app = angular.module("blocItOffApp", ["firebase"]);
-
-/* Now the $firebase service is available to be injected into any controller, service, or factory. */
-app.controller("blocItOffCtrl", ["$scope", "$firebase",
+var app = angular.module('blocItOff', ["firebase"]);
+app.controller("blocItOff.controller", ["$scope", "$firebase",
   function($scope, $firebase) {
-    var ref = new Firebase("https://blistering-heat-9377.firebaseIO.com/");
+    var ref = new Firebase("https://blistering-heat-9377.firebaseIO.com/data");
     // create an AngularFire reference to the data
     var sync = $firebase(ref);
     // download the data into a local object
-    $scope.data = sync.$asObject();
+    //$scope.data = sync.$asObject();
+
+    var syncObject = sync.$asObject();
+    syncObject.$bindTo($scope, "data");
+    // simple JSON output test for angular filter
+    //$scope.subText = "Current Tasks"; 
 
     // set up three-way data binding 
     // synchronize the object with a three-way data binding
