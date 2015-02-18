8593a8456f1b246fa01537eabd1c956f96232baa
Harry Hsu <hhsu@mindspring.com>
2015-02-18 05:04:13 +0000

    requirement 2 - see active task list on default page. 2-17-2015

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,8 @@
 /* Before we can use AngularFire with dependency injection, we need to register 
    firebase as a module in our app and defined in below line; Harry 2-08-2015*/
-var app = angular.module('blocItOff', ["firebase"]);
-app.controller("blocItOff.controller", ["$scope", "$firebase",
-  function($scope, $firebase) {
-    var ref = new Firebase("https://blistering-heat-9377.firebaseIO.com/data");
-   
-    // create an AngularFire reference to the data
-    var sync = $firebase(ref);
-
-    // download the data into a local object
-    // $scope.data = sync.$asObject();
-
-    var syncObject = sync.$asObject();
-
-    // synchronize the object with a three-way data binding
-    // click on `index.html` above to see it used in the DOM!
-    syncObject.$bindTo($scope, "data");
-
-    // Bind ng-model, and create a synchronized array for use in our HTML code, 
-    $scope.messages = sync.$asArray();
-  }
-]);
 
 /* The $firebase service is available to be injected into any controller now. */
-blocItOff = angular.module('blocItOff', ['ui.router']);
+var blocItOff = angular.module('blocItOff', ['ui.router', 'firebase']);
 
 blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
    $locationProvider.html5Mode(true);
@@ -35,6 +14,23 @@ blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider
    });
 }]);
 
-blocItOff.controller('index.controller', ['$scope', function($scope) {
-   $scope.subText = "Current Tasks";
+blocItOff.controller('index.controller', ['$scope', '$firebase', function($scope, $firebase) {
+
+    var ref = new Firebase("https://blistering-heat-9377.firebaseIO.com/tasks");
+   
+    // create an AngularFire reference to the data
+    var sync = $firebase(ref);
+
+    // download the data into a local object
+    // $scope.data = sync.$asObject();
+
+    var syncObject = sync.$asObject();
+
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    syncObject.$bindTo($scope, "tasks");
+
+    // Bind ng-model, and create a synchronized array for use in our HTML code, 
+    $scope.tasks = sync.$asArray();
+    $scope.subText = "Current Tasks";
 }]);
