ba29f3a2bd50ff5cd938dc87e383035e6c06ebda
Harry Hsu <hhsu@mindspring.com>
2015-03-24 17:05:54 +0000

    fixed filter:status default search issue; and update angularfire etc. dependencies and migrate from angularfire 0.9.2 to 1.0.0.

--- app/bak/app.js
+++ app/bak/app.js
@@ -39,28 +39,69 @@ blocItOff.config(['$stateProvider', '$locationProvider', function($stateProvider
 blocItOff.controller('home.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray, $filter) {
 
     var ref = new Firebase("https://blistering-heat-9377.firebaseIO.com/tasks");
+   
+    // create an AngularFire reference to the data
+    //var sync = $firebase(ref);
+    
+    //var sync = $firebaseArray(ref);  3-23-2015
+
+    // download the data into a local object
+    // $scope.data = sync.$asObject();
+
+    //var syncObject = sync.$asObject();
+
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    //syncObject.$bindTo($scope, "tasks");
+
+    // Bind ng-model, and create a synchronized array for use in our HTML code, 
+    //$scope.tasks = sync.$asArray();  3-23-15
 
     $scope.tasks = $firebaseArray(ref); // 3-23-15
     $scope.subText = "Current Tasks";
+
+    // Harry 3-23-2015
+    //$scope.searchObj = {status: "active"};
+    //$scope.searchCriteria = function(value,index) {
+    //    if (value.status === "active"){
+    //       return true
+    //    } 
+    //    else {
+    //         return false
+    //    }      
+    //}
 
 }]);
 
 blocItOff.controller('currentTask.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray, $filter) {
     var ref = new Firebase("https://blistering-heat-9377.firebaseIO.com/tasks");
    
-    // create an AngularFire reference to the data  
+    // create an AngularFire reference to the data
+    
+    //var sync = $firebaseArray(ref); 3-23-2015
+    //var syncObject = sync.$asObject(); 3-23-2015
+
+    // synchronize the object with a three-way data binding using the bindTo() method. 
+    // syncObject.$bindTo($scope, "tasks");  3-23-2015
+
+    // Bind ng-model, and create a Angular synchronized array for use in our HTML code, 
+    // $scope.tasks = sync.$asArray(); 3-23-2015
     $scope.tasks = $firebaseArray(ref);
     $scope.subText = "Current Tasks"; 
 
      $scope.completeTask = function(task) {
        task.status = 'inactive'; 
      };
+
+    //$scope.findActiveTask = function(task) {
+    //   return task.status === 'active';
+    //};
 }]);
 
 blocItOff.filter('findActiveTask', function($log) {
      return function(tasks) {
         //$log.info(tasks);
-    
+        //return (task.status === 'active');
         var activeTasks = [];
         for (var i=0; i<tasks.length; i++) {
           //$log.info(tasks[i]);
@@ -79,6 +120,16 @@ blocItOff.controller('taskHistory.controller', ['$scope', '$firebaseArray', func
     // create an AngularFire reference to the data
     var sync = $firebaseArray(ref);
 
+    // download the data into a local object
+    // $scope.data = sync.$asObject();
+
+    // var syncObject = sync.$asObject(); 3-23-2015
+
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    // syncObject.$bindTo($scope, "tasks");  3-23-2015
+
+    // Bind ng-model, and create a synchronized array for use in our HTML code, 
     $scope.tasks = $firebaseArray(ref);
     $scope.subText = "Task History";
 }]);
@@ -86,11 +137,14 @@ blocItOff.controller('taskHistory.controller', ['$scope', '$firebaseArray', func
 blocItOff.controller('addTask.controller', ['$scope', '$firebaseArray', function($scope, $firebaseArray, $filter) {
     var ref = new Firebase("https://blistering-heat-9377.firebaseIO.com/tasks");
     //var sync = $firebaseArray(ref); 3-23-2015
+
+    /* display current tasks here.  */
     // var syncObject = sync.$firebaseObject();  3-23-2015
     // syncObject.$bindTo($scope, "tasks");  3-23-2015
                         
     $scope.subText = "Add Task";  
 
+    //$scope.tasks = sync.$asArray();
     // $scope.messages = sync.$asArray();  // 3-23-2015
     $scope.messages = $firebaseArray(ref); // 3-23-2015
         
